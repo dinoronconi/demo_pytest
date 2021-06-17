9dae8847830b4d7c5379f6b8431058d45886ed35
Dino Ronconi <dronconi@practia.global>
2021-06-17 09:18:31 -0300

    Segun versión,más modularizado

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from prediccion_math import  implemento_modelo
 from operaciones import suma, resta
 
-print('Suma : ', suma(21, 5))
-print('Resta: ', resta(suma(21, 5), 5))
+miSuma = suma(21, 5)
+print('Suma : ', miSuma)
+print('Resta: ', resta(suma(miSuma,8), 5))
 
-print('R2: ',implemento_modelo('StudentsPerformance.csv'))
+print('R2: ',implemento_modelo('StudentsPerformance.csv', 0.33, 3, 8))
+
+
 
